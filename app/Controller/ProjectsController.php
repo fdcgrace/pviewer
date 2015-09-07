1e60da7d1202d9a4b834b63de3b121859c51abcf
Gracie <fdcgrace@gmail.com>
2015-09-07 09:10:21 +0800

    projects page (responsive table)

--- app/Controller/ProjectsController.php
+++ app/Controller/ProjectsController.php
@@ -8,7 +8,9 @@ App::uses('AppController', 'Controller');
  * @property SessionComponent $Session
  */
 class ProjectsController extends AppController {
+
 	public $uses = array('Project', 'Pdetail', 'Team');
+
 /**
  * Components
  *
@@ -22,6 +24,31 @@ class ProjectsController extends AppController {
  * @return void
  */
 	public function index() {
+		$this->Project->recursive = 0;
+		$this->paginate = array(
+	            'limit' => 5, 
+	            'order' => 'modified DESC', 
+	            'joins' => array(
+						array(
+							'table' => 'Pdetails',
+							'alias' => 'Pdetail',
+							'type'	=> 'LEFT',
+							'conditions' => array('Pdetail.project_id = Project.id AND Pdetail.status <> 6 AND Pdetail.status <> 5')
+						),
+						array(
+							'table' => 'Teams',
+							'alias' => 'Leader',
+							'type'	=> 'LEFT',
+							'conditions' => array('Project.team_id = Leader.id')
+						),
+
+					),
+	            'group' => array('Pdetail.project_id'),
+	            'fields' => array('count(Pdetail.project_id) as total_num_task', '*', 'Leader.team')
+        	);
+		
+		$pagination = $this->paginate('Project');
+		$this->set('projects', $pagination);
 	}
 
 /**
@@ -32,6 +59,11 @@ class ProjectsController extends AppController {
  * @return void
  */
 	public function view($id = null) {
+		if (!$this->Project->exists($id)) {
+			throw new NotFoundException(__('Invalid project'));
+		}
+		$options = array('conditions' => array('Project.' . $this->Project->primaryKey => $id));
+		$this->set('project', $this->Project->find('first', $options));
 	}
 
 /**
@@ -40,9 +72,24 @@ class ProjectsController extends AppController {
  * @return void
  */
 	public function add() {
-		$this->autoRender = false;
-		$view = new View($this, false);
-		return $view->element('newProj');
+		if ($this->request->is('post')) {
+			$this->Project->create();
+			if ($this->Project->save($this->request->data)) {
+				$this->Session->setFlash(__('The project has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+			}
+		}
+
+		$teams = $this->Project->Team->find('list', array(
+			'fields' => array(
+				'team'
+				)
+			)
+		);
+
+		$this->set(compact('teams'));
 	}
 
 /**
@@ -53,9 +100,29 @@ class ProjectsController extends AppController {
  * @return void
  */
 	public function edit($id = null) {
-		$this->autoRender = false;
-		$view = new View($this, false);
-		return $view->element('editProj');
+		if (!$this->Project->exists($id)) {
+			throw new NotFoundException(__('Invalid project'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Project->save($this->request->data)) {
+				$this->Session->setFlash(__('The project has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Project.' . $this->Project->primaryKey => $id));
+			$this->request->data = $this->Project->find('first', $options);
+		}
+
+		$teams = $this->Project->Team->find('list', array(
+			'fields' => array(
+				'team'
+				)
+			)
+		);
+
+		$this->set(compact('teams'));
 	}
 
 /**
@@ -66,5 +133,16 @@ class ProjectsController extends AppController {
  * @return void
  */
 	public function delete($id = null) {
+		$this->Project->id = $id;
+		if (!$this->Project->exists()) {
+			throw new NotFoundException(__('Invalid project'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Project->delete()) {
+			$this->Session->setFlash(__('The project has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The project could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
 	}
 }
